507aaad5045412411651f6de3d8a0b3293aa4d75
syoma <skat.apps@gmail.com>
2022-05-22 01:13:54 +0300

    first one splash screen

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -3,13 +3,15 @@ package com.example.testcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
-import androidx.compose.material.TopAppBar
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.testcompose.cookBook.RecipeList
 import com.example.testcompose.cookBook.defaultRecipes
 import com.example.testcompose.ui.theme.TestComposeTheme
@@ -18,12 +20,43 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent() {
-            TestComposeTheme() {
-                Column(modifier = Modifier
-                    .fillMaxSize()
-                    .background(MaterialTheme.colors.background)) {
-                    RecipeList(recipes = defaultRecipes)
+            TestComposeTheme(true) {
+                Column(
+                    verticalArrangement = Arrangement.SpaceBetween,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colors.background)
+                    ) {
+
+                    Column{
+                        TitleMakenna()
+                        SubTitleMakenna()
+                    }
+
+                    Image(
+                        painter = painterResource(id = R.drawable.ic_baseline_account_balance_24),
+                        contentDescription = "",
+                        Modifier.size(250.dp).padding(bottom = 32.dp)
+                    )
+
+                    Button(
+                        onClick = { /*TODO*/ },
+                        colors = ButtonDefaults.buttonColors(
+                            backgroundColor = colorResource(id = R.color.purple_200)
+                        ),
+                        modifier = Modifier
+                            .width(250.dp)
+                            .padding(16.dp)
+
+                    ) {
+                        Text(text = "dsfds")
+                    }
                 }
+
+
+
+
             }
 
         }
--- app/src/main/java/com/example/testcompose/WorkWithText.kt
+++ app/src/main/java/com/example/testcompose/WorkWithText.kt
@@ -1,23 +1,29 @@
 package com.example.testcompose
 
 import android.graphics.Color
+import android.text.style.StyleSpan
 import android.util.Log
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color.Companion.Blue
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@Preview
 @Composable
 fun myText() {
     Column {
@@ -29,7 +35,6 @@ fun myText() {
     }
 }
 
-@Preview
 @Composable
 fun AnnotatedClickableText() {
     val annotatedText = buildAnnotatedString {
@@ -68,4 +73,46 @@ fun AnnotatedClickableText() {
                 }
         }
     )
+}
+
+@Preview
+@Composable
+fun TitleMakenna() {
+    Text(
+        buildAnnotatedString {
+        withStyle(style = SpanStyle(
+            fontSize = 64.sp,
+            color = colorResource(id = R.color.white))
+        ) {
+            append("Makenna \n")
+        }
+        withStyle(
+            style = SpanStyle(
+                color = colorResource(id = R.color.purple_200),
+                fontSize = 68.sp)
+        ) {
+            append("Gallery")
+        }
+    },
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier.padding(top = 16.dp),
+        lineHeight = 32.sp
+    )
+}
+
+@Preview
+@Composable
+fun SubTitleMakenna() {
+    Text(
+        buildAnnotatedString {
+        withStyle(style = SpanStyle(fontSize = 24.sp, fontWeight = FontWeight.Bold)) {
+            append("18K ")
+        }
+        append("artworks")
+    },
+        fontSize = 18.sp,
+        textAlign = TextAlign.End,
+        color = colorResource(id = R.color.white),
+        modifier = Modifier.padding(start = 64.dp),
+        )
 }
